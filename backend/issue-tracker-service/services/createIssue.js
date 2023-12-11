150f0643aed5263ab4312a509a0030aeb3cf934a
abijith007 <trabijith@gmail.com>
2023-12-11 15:40:02 +0000

    Changes

--- backend/issue-tracker-service/services/createIssue.js
+++ backend/issue-tracker-service/services/createIssue.js
@@ -51,6 +51,7 @@ module.exports = createIssueService = async (issueDetails) => {
 
   let assignee_user = await Users.findOne({where: {userID: issueDetails.assignee}});
   let created_by_user = await Users.findOne({where: {userID: issueDetails.createdBy}});
+  
   axios.post(process.env.NOTIFICATION_SERVICE, {
     type: 'issue-created',
     title: issueDataValues.title,
--- services-execute.sh
+++ services-execute.sh
@@ -1,14 +1,15 @@
 kubectl apply -f ./backend/users-service/deployment.yaml
 kubectl apply -f ./backend/users-service/service.yaml
-
+sleep 60
 kubectl apply -f ./backend/notification-service/deployment.yaml
 kubectl apply -f ./backend/notification-service/service.yaml
-
+sleep 60
 kubectl apply -f ./backend/issue-tracker-service/deployment.yaml
 kubectl apply -f ./backend/issue-tracker-service/service.yaml
-
-sleep 10
-
+sleep 60
+kubectl apply -f ./backend/chatbot-service/deployment.yaml
+kubectl apply -f ./backend/chatbot-service/service.yaml
+sleep 60
 kubectl apply -f ./backend/api-gateway/deployment.yaml
 kubectl apply -f ./backend/api-gateway/service.yaml
 
