7857577c4c1ec734ac4d78e5ee6cb96671b84e76
Abijith Trichur Ramachandran <trabijith@gmail.com>
2023-12-12 21:22:24 -0700

    Fixed nav

--- frontend/src/components/common-components/NavBar/NavBar.js
+++ frontend/src/components/common-components/NavBar/NavBar.js
@@ -1,7 +1,6 @@
-// NavBar.js
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { logout } from '../../../redux/actions/userActions';
 import { useState } from 'react';
 
@@ -14,96 +13,35 @@ function NavBar() {
     navigate('/' + page);
   };
 
-  const handleLogout = () => {
-    dispatch(logout());
-    navigate('/');
-  };
-
-  const getDynamicBackgroundColor = () => {
-    const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    return isDarkMode ? '#34495e' : '#3498db';
-  };
+  const handleLogout = () => () => {
+    dispatch(logout());    
+    navigate("/");    
+  }
 
   return (
-    <div
-      style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        zIndex: 1000,
-        backgroundColor: getDynamicBackgroundColor(),
-        padding: '8px',
-        boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        transition: 'background-color 0.3s ease',
-      }}
-    >
-      <div className="ms-2">
-        <button
-          className="btn btn-sm btn-light normal-case text-xl"
-          style={{ fontWeight: 'bold', color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}
-          onClick={goTo('')}
-        >
-          Trackademic
-        </button>
+    <div className="navbar w-100 bg-blue-900">
+      <div className="flex-1 ms-2 align-middle">
+        <button className="btn  btn-sm btn-light normal-case align-middle text-center text-xl py-auto" onClick={goTo('')}>trackademic</button>
       </div>
-
-      {isLoggedIn ? (
-        <div className="flex gap-2 me-3">
+      { isLoggedIn ?
+        <div className="flex flex-row-reverse gap-2 me-10">
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-
               <div className="w-10 rounded-full">
                 <img src={profilePhoto != "" ? profilePhoto : "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg"} alt="User Avatar" />                
               </div>
             </label>
-            <ul
-              tabIndex={0}
-              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-primary-100 rounded-box w-36"
-            >
-              <li>
-                <button onClick={goTo('profile')}>Profile</button>
-              </li>
-              <li>
-                <button onClick={handleLogout}>Logout</button>
-              </li>
+            <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-primary-100 rounded-box w-52">
+              <li><button onClick={goTo('profile')}>Profile</button></li>
+              <li><button onClick={handleLogout()}>Logout</button></li>
             </ul>
           </div>
+        </div> : 
+        <div className="flex flex-row-reverse gap-2 me-5">
+          <button className='btn btn-danger  btn-sm' onClick={goTo('login')}>Login</button>
+          <button className='btn btn-warning  btn-sm' onClick={goTo('signup')}>Signup</button>
         </div>
-      ) : (
-        <div className="flex gap-2 me-3">
-          <div className="flex justify-center">
-            <button
-              className="btn btn-light btn-sm mr-1" // Added margin-right for spacing
-              onClick={goTo('signup')}
-            >
-              Sign Up
-            </button>
-            <button
-              className="btn btn-light btn-sm"
-              onClick={goTo('login')}
-            >
-              Login
-            </button>
-          </div>
-        </div>
-      )}
-
-      {/* Unchanged styles for "Feature," "Team," and "About Us" buttons */}
-      <div className="flex gap-2 me-3">
-        <button className="btn btn-light btn-sm" onClick={goTo('feature')}>
-          Feature
-        </button>
-        <button className="btn btn-light btn-sm" onClick={goTo('team')}>
-          Team
-        </button>
-        <button className="btn btn-light btn-sm" onClick={goTo('about')}>
-          About Us
-        </button>
-      </div>
+      }
     </div>
   );
 }
